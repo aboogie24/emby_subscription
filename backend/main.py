f153802fe31fc84b91c016e2d6cf32ab09f24103
aboogie24 <alfred.brown@alfredbrowniii.io>
2025-08-05 14:54:20 -0400

    Emby-Sub: Updating deployment

--- backend/main.py
+++ backend/main.py
@@ -88,7 +88,7 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
     }
 
     r = requests.post(url, headers=headers, json=payload)
-    print(r.data)
+    print(r.json())
     if r.status_code != 200:
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
